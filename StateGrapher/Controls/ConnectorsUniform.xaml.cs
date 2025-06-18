f2ec459db9729a988adbf450f2101c6e4f940a2e
Artem Pindrus <artempindrus@gmail.com>
2025-06-18 19:22:50 +0300

    Custom control for connectors

---
+++ StateGrapher/Controls/ConnectorsUniform.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace StateGrapher.Controls {
+    /// <summary>
+    /// Interaction logic for ConnectorsUniform.xaml
+    /// </summary>
+    public partial class ConnectorsUniform : UserControl {
+        public int Columns { get; set; }
+        public int Rows { get; set; }
+
+        public ConnectorsUniform() {
+            InitializeComponent();
+        }
+    }
+}
