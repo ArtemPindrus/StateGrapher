adedbeb5eb5829e86aba67446cacc087ebcae151
Artem Pindrus <artempindrus@gmail.com>
2025-06-18 13:53:24 +0300

    Tests

---
+++ StateGrapher.Tests/StateMachineTests.cs
@@ -0,0 +1,31 @@
+﻿using StateGrapher.Extensions;
+using StateGrapher.Models;
+using StateGrapher.Utilities;
+
+namespace StateGrapher.Tests {
+    public class StateMachineTests {
+        [Fact]
+        public void GetLeastCommonAncestor_ReturnsValidNode() {
+            StateMachine root = new StateMachine() { Name = "Root"}.TryAddNodes(
+                new StateMachine() { Name = "Standing" },
+                new StateMachine() { Name = "RunningLock" }.TryAddNodes(
+                    new StateMachine() { Name = "IsUncrouching" }
+                )
+            );
+
+            root.TryAddConnection("Standing", "RunningLock").ForwardEvent = "CTRL";
+            var connection = root.TryAddConnection("RunningLock", "Standing");
+            connection.ForwardEvent = "CTRL";
+
+            Assert.NotNull(connection);
+
+            List<Transition> transitions = StateMachineUtility.ToTransitions([connection]);
+
+            Assert.True(transitions.Count > 0);
+
+            var lca = StateMachineUtility.GetLeastCommonAncestor(transitions[0]);
+
+            Assert.True(lca.Name == "Root");
+        }
+    }
+}
--- StateGrapher/Extensions/StateMachineExtensions.cs
+++ StateGrapher/Extensions/StateMachineExtensions.cs
@@ -20,6 +20,26 @@ namespace StateGrapher.Extensions
             }
         }
 
+        public static Connection? TryAddConnection(this StateMachine root, string fromState, string toState) {
+            StateMachine? from = root.Nodes.FirstOrDefault(x => x.Name == fromState) as StateMachine;
+            StateMachine? to = root.Nodes.FirstOrDefault(x => x.Name == toState) as StateMachine;
+
+            if (from != null && to != null) {
+                return root.TryAddConnection(from.Connectors.TopConnectors[0], to.Connectors.TopConnectors[0]);
+            }
+
+            return null;
+        }
+
+        public static IEnumerable<StateMachine> GetParentTree(this StateMachine sm) {
+            var parent = sm.Container;
+
+            while (parent != null) {
+                yield return parent;
+                parent = parent.Container;
+            }
+        }
+
         /// <summary>
         /// Get all <see cref="StateMachine"/> nodes.
         /// </summary>
@@ -34,6 +54,14 @@ namespace StateGrapher.Extensions
 
                 foreach (var child in GetHierarchyNodes(node)) yield return child;
             }
+        }
+
+        public static StateMachine TryAddNodes(this StateMachine sm, params Node[] nodes) {
+            foreach (var node in nodes) {
+                sm.TryAddNode(node);
+            }
+
+            return sm;
         }
 
         public static IEnumerable<Connection> GetHierarchyConnections(this StateMachine sm) {
