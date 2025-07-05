293591a897617ba6a994eaeb589bf6934d54ad5f
Artem Pindrus <artempindrus@gmail.com>
2025-07-05 18:13:37 +0300

    nicer tostring()

--- StateGrapher/Models/Transition.cs
+++ StateGrapher/Models/Transition.cs
@@ -1,5 +1,27 @@
-﻿namespace StateGrapher.Models {
+﻿using System.Text;
+
+namespace StateGrapher.Models {
     public record struct Transition(string Name, StateMachine From, StateMachine To, StateMachine Container, IList<ConnectionCondition> Conditions) {
-        public override string ToString() => $"{From.Name} > {Name} > {To.Name}";
+        public readonly string GetConditionsString() {
+            if (Conditions.Count == 0) return "";
+
+            StringBuilder s = new("if (");
+
+            bool firstIteration = true;
+            foreach (var condition in Conditions) {
+                if (!firstIteration) s.Append(" && ");
+
+                if (!condition.ShouldBeTrue) s.Append('!');
+                s.Append(condition.SmBoolean.Name);
+
+                firstIteration = false;
+            }
+
+            s.Append(')');
+
+            return s.ToString();
+        }
+        
+        public readonly override string ToString() => $"{From.Name} > {GetConditionsString()} > through {Name} > {To.Name}";
     }
 }
